e3c32e36f5ed22c5106b0b0b0b21c6717837e911
aud404_st <aud404_st@CR404-PC12>
2023-04-28 19:04:10 +0300

    Добавьте файлы проекта.

---
+++ regex_01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        string pattern = new string(@"(?=\S*\d\S*\d\S*\d)[\S\d]{9,}");
+        Regex regex = new Regex(pattern);
+
+        while (true)
+        {
+            Console.WriteLine("придумайте пароль:");
+            var isValid = regex.IsMatch(Console.ReadLine());
+
+            if (isValid)
+            {
+                Console.Clear();
+                Console.WriteLine("пароль прийнято!");
+                break;
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("невірний формат пароля!\n(пароль має складатися мінімум з 9 символів та 3 цифр)\n");
+            }
+        }
+    }
+}
